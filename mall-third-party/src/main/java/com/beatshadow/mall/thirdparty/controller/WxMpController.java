4fcd1e33de63c378c1472091530f6e545f1354f4
BeatShadow <gnehcgnaw@gmail.com>
2021-11-02 18:48:27 +0800

    wx message push
    Signed-off-by: BeatShadow <gnehcgnaw@gmail.com>

--- mall-third-party/src/main/java/com/beatshadow/mall/thirdparty/controller/WxMpController.java
+++ mall-third-party/src/main/java/com/beatshadow/mall/thirdparty/controller/WxMpController.java
@@ -1,5 +1,19 @@
-package com.beatshadow.mall.thirdparty.controller;/**
+package com.beatshadow.mall.thirdparty.controller;
+
+import com.beatshadow.common.utils.R;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
  * @author gnehcgnaw
- * @date 2021-10-13 10:10 
- */public class WxMpController {
+ * @date 2021-10-13 10:10
+ */
+@RestController
+@RequestMapping("/wx-mp")
+public class WxMpController {
+    @GetMapping("/token")
+    public R token(){
+        return null ;
+    }
 }
