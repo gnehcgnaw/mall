2333de489b0b09d27389a93806e4be3c8163c705
BeatShadow <gnehcgnaw@gmail.com>
2021-11-02 18:48:15 +0800

    wx message push
    wx message push
    
    Signed-off-by: BeatShadow <gnehcgnaw@gmail.com>

--- mall-wx/src/main/java/com/beatshadow/mallwx/controller/WxMpController.java
+++ mall-wx/src/main/java/com/beatshadow/mallwx/controller/WxMpController.java
@@ -1,5 +1,23 @@
-package com.beatshadow.mallwx.controller;/**
+package com.beatshadow.mallwx.controller;
+
+import com.beatshadow.common.utils.R;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
  * @author gnehcgnaw
- * @date  10:41 
- */public class WxMpController {
+ * @date 10:41
+ */
+@RequestMapping("wx-mp")
+@RestController
+public class WxMpController {
+    @GetMapping("/token")
+    public String token(@RequestParam(value = "signature") String signature,
+                   @RequestParam(value = "timestamp") String timestamp,
+                   @RequestParam(value = "nonce") String nonce,
+                   @RequestParam(value = "echostr") String echostr){
+        return echostr ;
+    }
 }
--- mall-wx/src/test/java/com/beatshadow/mallwx/wxmp/WxMpTest.java
+++ mall-wx/src/test/java/com/beatshadow/mallwx/wxmp/WxMpTest.java
@@ -1,28 +1,65 @@
-package com.beatshadow.mall.thirdparty.wxmp;
+package com.beatshadow.mallwx.wxmp;
 
+import me.chanjar.weixin.common.error.WxErrorException;
 import me.chanjar.weixin.mp.api.WxMpService;
 import me.chanjar.weixin.mp.api.impl.WxMpServiceImpl;
+import me.chanjar.weixin.mp.bean.result.WxMpUser;
+import me.chanjar.weixin.mp.bean.result.WxMpUserList;
+import me.chanjar.weixin.mp.bean.template.WxMpTemplateData;
+import me.chanjar.weixin.mp.bean.template.WxMpTemplateMessage;
 import me.chanjar.weixin.mp.config.impl.WxMpDefaultConfigImpl;
+import org.junit.Before;
+import org.junit.Test;
 
 /**
  * @author gnehcgnaw
  * @date 10:04
  */
 public class WxMpTest {
-    //http://beatshadow.natapp1.cc/wx-mp/token
-    //mytoken
     private final static String appId = "wx9d2319eccc44338d";
     private final static String appSecret = "cabbb4d3e9451a0d6f9af000b65812e4";
-    public void test1(){
+    private  WxMpService wxService ;
+
+    @Before
+    public void init(){
         WxMpDefaultConfigImpl wxMpDefaultConfig = new WxMpDefaultConfigImpl();
         wxMpDefaultConfig.setAppId(appId); // 设置微信公众号的appid
         wxMpDefaultConfig.setSecret(appSecret); // 设置微信公众号的app corpSecret
-        wxMpDefaultConfig.setToken("..."); // 设置微信公众号的token
+        wxMpDefaultConfig.setToken("mytoken"); // 设置微信公众号的token
         wxMpDefaultConfig.setAesKey("..."); // 设置微信公众号的EncodingAESKey
-
-        WxMpService wxService = new WxMpServiceImpl();// 实际项目中请注意要保持单例，不要在每次请求时构造实例，具体可以参考demo项目
+        wxService = new WxMpServiceImpl();// 实际项目中请注意要保持单例，不要在每次请求时构造实例，具体可以参考demo项目
         wxService.setWxMpConfigStorage(wxMpDefaultConfig);
+    }
+
+    /**
+     * 获取公众号用户列表，即所有用户的公众号openid
+     * @throws WxErrorException
+     */
+    @Test
+    public void userList() throws WxErrorException {
+        WxMpUserList wxMpUserList = wxService.getUserService().userList(null);
+        System.out.println(wxMpUserList);
+    }
 
 
+    /**
+     * 通过公众号openid获取用户的unionid
+     */
+    @Test
+    public void userInfo() throws WxErrorException {
+        WxMpUser wxMpUser = wxService.getUserService().userInfo("ogA155s92NQoiJWi7XvLbsNW8cq0");
+        System.out.println(wxMpUser);
+    }
+
+    /**
+     * 推送服务号消息
+     * @throws WxErrorException
+     */
+    @Test
+    public void sendTemplateMsg() throws WxErrorException {
+        WxMpTemplateMessage wxMpTemplateMessage = WxMpTemplateMessage.builder().toUser("ogA155s92NQoiJWi7XvLbsNW8cq0").templateId("0uoAUqcg5koYGo-oOG3oF81Fe-ebFVFc1ycaLDjUqY4").build();
+        wxMpTemplateMessage.addData(new WxMpTemplateData("name", "张三", "#191970"));
+        wxMpTemplateMessage.addData(new WxMpTemplateData("sex", "男", "#191970"));
+        wxService.getTemplateMsgService().sendTemplateMsg(wxMpTemplateMessage);
     }
 }
